bbde947c13091c8c3cfc07992db954671a10b36d
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-10-16 23:30:17 +0100

    refactored

--- src/Components/listMovies/index.js
+++ src/Components/listMovies/index.js
@@ -27,37 +27,39 @@ export default function ListMovies() {
         <h1 className="font-weight-light text-center">Movies</h1>
         <div className="container mt-4">
           <div className="row d-flex justify-content-around">
-            {movies.map(
-              ({ title, short_description, duration, duration_seconds }) => {
-                return (
-                  <div className="col-12 mb-3" key={title}>
-                    <div className="card">
-                      <img src="" alt="" />
-                      <div className="card-body">
-                        <h4 className="card-title">
-                          <b>{title}</b>
-                        </h4>
-                        <span>
-                          <b>
-                            <p>{short_description}</p>
-                          </b>
-                        </span>
-                        <span>
-                          <b>
-                            <p>{duration}</p>
-                          </b>
-                        </span>
-                        <span>
-                          <b>
-                            <p>{duration_seconds} seconds</p>
-                          </b>
-                        </span>
+            {movies
+              .sort((a, b) => (a.title > b.title ? 1 : -1))
+              .map(
+                ({ title, short_description, duration, duration_seconds }) => {
+                  return (
+                    <div className="col-12 mb-3" key={title}>
+                      <div className="card">
+                        <img src="" alt="" />
+                        <div className="card-body">
+                          <h4 className="card-title">
+                            <b>{title}</b>
+                          </h4>
+                          <span>
+                            <b>
+                              <p>{short_description}</p>
+                            </b>
+                          </span>
+                          <span>
+                            <b>
+                              <p>{duration}</p>
+                            </b>
+                          </span>
+                          <span>
+                            <b>
+                              <p>{duration_seconds} seconds</p>
+                            </b>
+                          </span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                );
-              }
-            )}
+                  );
+                }
+              )}
           </div>
         </div>
       </div>
